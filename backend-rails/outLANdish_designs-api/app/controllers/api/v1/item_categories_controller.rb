830fdc2e0f67f5b3c72a34a3330dee3466922fc4
lanibon <nguyenlan13@gmail.com>
2019-12-11 21:14:02 -0800

    add api namespacing

---
+++ backend-rails/outLANdish_designs-api/app/controllers/api/v1/item_categories_controller.rb
@@ -0,0 +1,2 @@
+class Api::V1::ItemCategoriesController < ApplicationController
+end
---
+++ backend-rails/outLANdish_designs-api/app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,38 @@
+class Api::V1::ItemsController < ApplicationController
+
+    def index
+        @items = Item.all
+        render json: @items, status: 200
+    end
+
+    def create
+        @item = Item.create(item_params)
+
+        render json: @item, status: 200
+    end
+
+    def show
+        @item = Item.find(params[:id])
+
+        render json: @item, status: 200
+    end
+
+    def update
+        @item = Item.find(params[:id])
+        @item.update(item_params)
+        render json: @item, status: 200
+    end
+
+    def destroy
+        @item = Item.find(params[:id])
+        @item.delete
+    
+        render json: {itemId: @item.id}
+    end
+
+    private
+    def item_params
+        params.require(:item).permit()
+    end
+end
+
