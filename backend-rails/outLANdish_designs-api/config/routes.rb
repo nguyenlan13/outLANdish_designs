58c98c17da18dae3b4b2fe63be29381082a1dad8
lanibon <nguyenlan13@gmail.com>
2019-12-11 21:17:50 -0800

    add new resource

--- backend-rails/outLANdish_designs-api/config/routes.rb
+++ backend-rails/outLANdish_designs-api/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :orders
+  resources :accounts
   resources :wishlists
   resources :categories
   resources :reviews
-  resources :customers
   resources :items
   resources :carts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
