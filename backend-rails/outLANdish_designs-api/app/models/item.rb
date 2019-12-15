ccbd87b555f607ba6f89417e76d2bbf6ef3eee29
lanibon <nguyenlan13@gmail.com>
2019-12-14 22:34:19 -0800

    add associations

--- backend-rails/outLANdish_designs-api/app/models/item.rb
+++ backend-rails/outLANdish_designs-api/app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
 
     has_many :reviews
     has_many :wishlist_items
-    item has_many :wishlists, through: :wishlist_items
+    has_many :wishlists, through: :wishlist_items
     has_many :item_categories
     has_many :categories, through: :item_categories
 end
--- backend-rails/outLANdish_designs-api/app/models/wishlist.rb
+++ backend-rails/outLANdish_designs-api/app/models/wishlist.rb
@@ -1,2 +1,5 @@
 class Wishlist < ApplicationRecord
+    belongs_to :account
+    has_many :wishlist_items
+    item has_many :items, through: :wishlist_items
 end
