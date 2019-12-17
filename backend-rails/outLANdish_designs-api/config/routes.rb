6277c0b51610d662d7bf3513db2b1e1606a9531a
lanibon <nguyenlan13@gmail.com>
2019-12-16 19:11:03 -0800

    add nested routes

--- backend-rails/outLANdish_designs-api/config/routes.rb
+++ backend-rails/outLANdish_designs-api/config/routes.rb
@@ -4,12 +4,36 @@ namespace :api do
     namespace :v1 do
         resources :orders
         resources :accounts
-        # resources :wishlists
-        # resources :categories
-        # resources :reviews
+        resources :wishlists
+        resources :categories
+        resources :reviews
         resources :items
         resources :carts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+        end
+    end
+
+    namespace :api do
+        namespace :v1 do
+            resources :items do
+                resources :categories
+            end
+        end
+    end
+
+    namespace :api do
+        namespace :v1 do
+            resources :carts do
+                resources :items
+            end
+        end
+    end
+
+    namespace :api do
+        namespace :v1 do
+            resources :items do
+                resources :reviews
+            end
         end
     end
 end
