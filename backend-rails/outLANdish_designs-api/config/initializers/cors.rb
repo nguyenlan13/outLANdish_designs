4a30c43ed367b74c6cee8fc7d5d1ac74c9758b74
lanibon <nguyenlan13@gmail.com>
2019-12-21 23:55:12 -0800

    add configuration for cookies

--- backend-rails/outLANdish_designs-api/config/initializers/cors.rb
+++ backend-rails/outLANdish_designs-api/config/initializers/cors.rb
@@ -6,11 +6,13 @@
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+# Rails.application.config.action_controller.forgery_protection_origin_check = false, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3000'
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
 end
