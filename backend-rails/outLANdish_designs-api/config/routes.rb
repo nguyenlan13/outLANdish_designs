b232222d522f0bed84e03b13f81fa814da96a651
lanibon <nguyenlan13@gmail.com>
2019-12-24 09:24:09 -0800

    reorganize routes

--- backend-rails/outLANdish_designs-api/config/routes.rb
+++ backend-rails/outLANdish_designs-api/config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   
 namespace :api do
     namespace :v1 do
-        resources :orders
-        resources :accounts
-        resources :wishlists
+        # resources :orders
+        # resources :accounts
+        # resources :wishlists
         resources :categories
-        resources :reviews
+        # resources :reviews
         resources :items
-        resources :carts
+        # resources :carts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
         end
     end
@@ -17,13 +17,21 @@ namespace :api do
         namespace :v1 do
             resources :items do
                 resources :categories
+                resources :reviews
+            end
+            resources :accounts do
+                resources :orders
+                resources :reviews
+                resources :wishlists, only: [:show]
             end
             resources :carts do
                 resources :items
             end
-            resources :items do
-                resources :reviews
-            end
+            get "/signup" => "accounts#new", as: "signup"
+            post "/signup" => "accounts#create"
+            get "/login" => "sessions#new", as: "login"
+            post "/login" => "sessions#create"
+            delete "/logout" => "sessions#destroy"
         end
     end
 
@@ -43,13 +51,13 @@ namespace :api do
     #     end
     # end
     
-    namespace :api do
-        namespace :v1 do
-            get "/signup" => "accounts#new", as: "signup"
-            get "/login" => "sessions#new", as: "login"
-            post "/login" => "sessions#create"
-            delete "/logout" => "sessions#destroy"
-        end
-    end
+    # namespace :api do
+    #     namespace :v1 do
+    #         get "/signup" => "accounts#new", as: "signup"
+    #         get "/login" => "sessions#new", as: "login"
+    #         post "/login" => "sessions#create"
+    #         delete "/logout" => "sessions#destroy"
+    #     end
+    # end
 
 end
