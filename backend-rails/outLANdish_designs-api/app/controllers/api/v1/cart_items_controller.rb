209fdad1c3beb7d6a7425d169bfadcb213acecdb
lanibon <nguyenlan13@gmail.com>
2019-12-24 09:22:19 -0800

    add item to cart functionality

--- backend-rails/outLANdish_designs-api/app/controllers/api/v1/cart_items_controller.rb
+++ backend-rails/outLANdish_designs-api/app/controllers/api/v1/cart_items_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::CartItemsController < ApplicationController
+
+
+
+    def create
+        cart_item = CartItem.new(cart: Cart.find(params[:cart_id]), item: Item.find(params[:item_id]))
+        if cart_item.save
+            render json: cart_item, status 200
+        else
+            render json: { message: "Sorry, item could not be added to cart. Please try again."}, status: 400 
+        end
+    end
+
+    def destroy
+        cart_item = CartItem.find_by(cart: params[:cart_id], item: params[:item_id])
+        if cart_item.delete
+            render json: status 200
+        else
+            render json: { message: "Sorry, item was not removed from cart. Please try again."}, status: 400 
+        end
+    end
+
+
 end
