2893a9309557f2d47b68321cfc0dfe77a82abdc2
lanibon <nguyenlan13@gmail.com>
2020-01-04 16:27:42 -0800

    correct seed file for itemcategory

--- backend-rails/outLANdish_designs-api/db/seeds.rb
+++ backend-rails/outLANdish_designs-api/db/seeds.rb
@@ -12,6 +12,7 @@ lan_cart = Cart.create(account: lan)
 
 heapify = Item.create(name: "Heapify", description: 'harry potter inspired, min heap', color: "maroon", size: "xs", price: 24.99, item_path: "assets/Christmas_Maroon_Heapify.png", in_stock?: true)
 christmas = Item.create(name: "Binary", description: 'Binary Christmas Tree', color: "red", size: "xs", price: 19.99, item_path: "assets/binarytree_christmas_mockup.png", in_stock?: true)
+christmas2 = Item.create(name: "Santa", description: 'JS Santa', color: "red", size: "xs", price: 19.99, item_path: "assets/Santa_js_christmas_mockup.png", in_stock?: true)
 
 heapify_cart = CartItem.create(cart: lan_cart, item: heapify, quantity: 2)
 christmas_cart = CartItem.create(cart: lan_cart, item: christmas, quantity: 1)
@@ -20,10 +21,12 @@ nerdy = Category.create(name: "Nerdy")
 holiday = Category.create(name: "Holiday")
 music = Category.create(name: "Music")
 fitness = Category.create(name: "Fitness")
-inspiration = Category.create(name: "Inspiration")
+inspirational = Category.create(name: "Inspirational")
+sale = Category.create(name: "Sale")
 
-nerdy_item = ItemCategory.create(item_id: heapify, category_id: nerdy)
-holiday_item = ItemCategory.create(item_id: christmas, category_id: holiday)
+nerdy_item = ItemCategory.create(item: heapify, category: nerdy)
+holiday_item = ItemCategory.create(item: christmas, category: holiday)
+holiday_item2 = ItemCategory.create(item: christmas2, category: holiday)
 
 lan_order = Order.create(account:lan, order_date: "12/22/19", po_number: "LN122219", shipping_cost: 5.99, order_total: 25.99, billing_street_1: "123 fake street", billing_city: "irvine", billing_state: "CA", billing_zip: 92614, shipping_street_1: "321 fake street", shipping_city: "huntington beach", shipping_state: "CA", shipping_zip: 92649)
 lan_order_items = OrderItem.create(item: heapify, order: lan_order, item_name: "heapify", item_description: 'harry potter inspired, min heap', item_color: "maroon", item_size: "xs", item_price: 24.99, quantity: 1)
