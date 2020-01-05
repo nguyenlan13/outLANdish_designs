4c5d52d7651192ac7b7d58d0ec7200b3684fc609
lanibon <nguyenlan13@gmail.com>
2020-01-04 16:30:37 -0800

    rearrange routes

--- backend-rails/outLANdish_designs-api/config/routes.rb
+++ backend-rails/outLANdish_designs-api/config/routes.rb
@@ -1,35 +1,42 @@
 Rails.application.routes.draw do
   
-namespace :api do
-    namespace :v1 do
-        # resources :orders
-        # resources :accounts
-        # resources :wishlists
-        resources :categories
-        # resources :reviews
-        resources :items
-        # resources :carts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-        end
-    end
+# namespace :api do
+#     namespace :v1 do
+#         # resources :orders
+#         # resources :accounts
+#         # resources :wishlists
+#         resources :categories
+#         # resources :reviews
+#         resources :items
+#         # resources :carts
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#         end
+#     end
 
     namespace :api do
         namespace :v1 do
+           
             resources :items do
-                resources :categories
+                # resources :categories
                 resources :reviews
             end
+
             resources :categories do
                 resources :items
             end
+
             resources :accounts do
                 resources :orders
                 resources :reviews
                 resources :wishlists, only: [:show]
             end
+
             resources :carts do
                 resources :items
             end
+
+            # resources :categories
+            # resources :items
             get "/auth" => 'sessions#auth'
             get "/signup" => "accounts#new", as: "signup"
             post "/signup" => "accounts#create"
